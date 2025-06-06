18b12f0c3e5a7ac50959f23877fde1746a2180f3
Xavieroonga <xaangelicas@gmail.com>
2025-06-06 12:46:34 -0400

    yes

--- Script_git.R
+++ Script_git.R
@@ -5,3 +5,21 @@ DF = read_csv()
 
 
 Df = filter()
+
+
+#intentar subir proyecto a git_hub_________________>
+
+usethis::create_github_token()
+
+usethis::use_github()
+
+#listo!!
+
+#thestear el pusch con cambios para hacer una foto y crear un nodo en el master
+
+usethis::pr_init(branch = "Master")
+usethis::pr_push()
+
+
+
+
